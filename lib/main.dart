a16995d3e756a88977a1da55a7e5f3297258eaa6
Hamburgir <shantnuprofid@gmail.com>
2025-06-12 16:48:00 +0530

    Added drawer and system theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,239 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(
+    child: const App(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+
+        if (lightDynamic != null && darkDynamic != null) {
+          // On Android S+ devices, use the provided dynamic color scheme.
+          // (Recommended) Harmonize the dynamic color scheme' built-in semantic colors.
+          lightColorScheme = lightDynamic.harmonized();
+          // (Optional) Customize the scheme as desired. For example, one might
+          // want to use a brand color to override the dynamic [ColorScheme.secondary].
+          // lightColorScheme = lightColorScheme.copyWith(secondary: _brandBlue);
+          // (Optional) If applicable, harmonize custom colors.
+          // lightCustomColors = lightCustomColors.harmonized(lightColorScheme);
+
+          // Repeat for the dark color scheme.
+          darkColorScheme = darkDynamic.harmonized();
+          // darkColorScheme = darkColorScheme.copyWith(secondary: _brandBlue);
+          // darkCustomColors = darkCustomColors.harmonized(darkColorScheme);
+
+          // _isDemoUsingDynamicColors = true; // ignore, only for demo purposes
+        } else {
+          // Otherwise, use fallback schemes.
+          lightColorScheme = ColorScheme.fromSeed(
+            seedColor: Colors.blueAccent,
+          );
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: Colors.blueAccent,
+            brightness: Brightness.dark,
+          );
+        }
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "NoteQuest",
+          theme: ThemeData(
+            // primaryColor: MaterialAccentColor(accentColor),
+            colorScheme: lightColorScheme,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme,
+          ),
+          themeMode: ThemeMode.system,
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({
+    super.key,
+  });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  final _advancedDrawerController = AdvancedDrawerController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    int currentScreen = ref.watch(indexStorageProvider);
+    var currentScreenNotifier = ref.read(indexStorageProvider.notifier);
+    appBar(
+      Widget title, [
+      List<Widget> actions = const [],
+    ]) =>
+        AppBar(
+          title: title,
+          leading: IconButton(
+            onPressed: _handleMenuButtonPressed,
+            icon: ValueListenableBuilder<AdvancedDrawerValue>(
+              valueListenable: _advancedDrawerController,
+              builder: (_, value, __) {
+                return AnimatedSwitcher(
+                  duration: Duration(milliseconds: 250),
+                  child: Icon(
+                    value.visible ? Icons.clear : Icons.menu,
+                    key: ValueKey<bool>(value.visible),
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+          actions: actions,
+        );
+    var screens = <Widget>[
+      Scaffold(
+        appBar: appBar(Text('1')),
+        body: Text('placeholder'),
+        floatingActionButton: FloatingActionButton(
+          shape: CircleBorder(),
+          onPressed: () => print("Hello"),
+          child: Icon(Icons.add),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ];
+
+    return AdvancedDrawer(
+      backdrop: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Theme.of(context).focusColor,
+        // decoration: BoxDecoration(
+        //   gradient: LinearGradient(
+        //     begin: Alignment.topLeft,
+        //     end: Alignment.bottomRight,
+        //     colors: [Colors.blueGrey, Colors.blueGrey.withOpacity(0.2)],
+        //   ),
+        // ),
+      ),
+      controller: _advancedDrawerController,
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: const Duration(milliseconds: 500),
+      animateChildDecoration: true,
+      rtlOpening: false,
+      openScale: 0.83,
+      openRatio: 0.6,
+      disabledGestures: false,
+      childDecoration: const BoxDecoration(
+        // NOTICE: Uncomment if you want to add shadow behind the page.
+        // Keep in mind that it may cause animation jerks.
+        // boxShadow: <BoxShadow>[
+        //   BoxShadow(
+        //     color: Colors.black12,
+        //     blurRadius: 1.0,
+        //   ),
+        // ],
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      drawer: SafeArea(
+        child: ListTileTheme(
+          textColor: Colors.white,
+          iconColor: Colors.white,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Spacer(),
+              ListTile(
+                onTap: () {
+                  if (currentScreen != 0) {
+                    setState(() {
+                      currentScreenNotifier.update(0);
+                    });
+                  }
+                },
+                // leading: Icon(Icons.notes),
+                title: Text('Overview'),
+              ),
+              ListTile(
+                onTap: () {
+                  if (currentScreen != 1) {
+                    setState(() {
+                      currentScreenNotifier.update(1);
+                    });
+                  }
+                },
+                // leading: Icon(Icons.folder),
+                title: Text('Documents'),
+              ),
+              ListTile(
+                onTap: () {
+                  if (currentScreen != 2) {
+                    setState(() {
+                      currentScreenNotifier.update(2);
+                    });
+                  }
+                },
+                // leading: Icon(Icons.calendar_month),
+                title: Text('Calendar'),
+              ),
+              ListTile(
+                onTap: () {
+                  if (currentScreen != 3) {
+                    setState(() {
+                      currentScreenNotifier.update(2);
+                    });
+                  }
+                },
+                // leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+              Spacer(flex: 4),
+            ],
+          ),
+        ),
+      ),
+      child: screens[currentScreen],
     );
+  }
+
+  void _handleMenuButtonPressed() {
+    _advancedDrawerController.showDrawer();
+  }
+}
+
+@riverpod
+class IndexStorage extends _$IndexStorage {
+  @override
+  int build() {
+    // Initial value
+    return 0;
+  }
+
+  void update(int value) {
+    state = value;
+  }
+
+  void increment() {
+    state++;
+  }
+
+  void decrement() {
+    state--;
   }
 }
