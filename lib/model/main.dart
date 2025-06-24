9dda92fa09a210c7fdc4038a884c976a49e61214
Hamburgir <shantnuprofid@gmail.com>
2025-06-25 05:27:54 +0530

    Working on database serving provider

--- lib/model/main.dart
+++ lib/model/main.dart
@@ -1,53 +1,26 @@
-// useless mp
-export 'entities/budget.dart';
-export 'entities/expenses.dart';
-export 'entities/person.dart';
+import 'package:expensetracker/model/entities/budget.dart';
+import 'package:expensetracker/model/entities/category.dart';
+import 'package:expensetracker/model/entities/debt.dart';
+import 'package:expensetracker/model/entities/expenses.dart';
+import 'package:expensetracker/model/entities/person.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-// @riverpod
-// class Person extends _$Person {
-//     @override
-//   Future<List<PersonModel>> build() async {
-//         return [];
-//   }
-// }
-//
-// @riverpod
-// class Transactions extends _$Transactions {
-//     @override
-//   Future<Map<String, List<TransactionModel>>> build() async {
-//         return fetch();
-//   }
-//
-//   Future<Map<String, List<TransactionModel>>> updateState() async {
-//         return {};
-//   }
-//
-//   fetch() {
-//         return;
-//   }
-// }
-//
-// @riverpod
-// class Debts extends _$Debts {
-//     @override
-//   Future<List<TransactionModel>> build() async {
-//         return [];
-//   }
-// }
-//
-// @riverpod
-// class Budget extends _$Budget {
-//     @override
-//   Future<List<Budget>> build() async {
-//         return [];
-//   }
-// }
-//
-// @riverpod
-// class Category extends _$Category {
-//     @override
-//   Future<List<Category>> build() async {
-//         return [];
-//   }
-// }
-//
+part 'main.g.dart';
+
+class _DataStore {
+  List<Person> person = [];
+  List<Expense> expense = [];
+  List<Debt> debt = [];
+
+  List<Category> category = [];
+  List<Budget> budget = [];
+}
+
+@riverpod
+class Database extends _$Database {
+  _DataStore _dataStore = _DataStore();
+  @override
+  Future<_DataStore> build() async {
+    return _dataStore;
+  }
+}
---
+++ lib/model/main.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$databaseHash() => r'2069c21b72152feda4bc8749c143fc9b0c4555bc';
+
+/// See also [Database].
+@ProviderFor(Database)
+final databaseProvider =
+    AutoDisposeAsyncNotifierProvider<Database, _DataStore>.internal(
+  Database.new,
+  name: r'databaseProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$databaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Database = AutoDisposeAsyncNotifier<_DataStore>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
