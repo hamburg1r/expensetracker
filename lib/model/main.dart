306ad19410325fd1e1eaec341e67da2284484636
Hamburgir <shantnuprofid@gmail.com>
2025-06-27 16:46:23 +0530

    Added 3 providers for databases

--- lib/model/main.dart
+++ lib/model/main.dart
@@ -3,24 +3,49 @@ import 'package:expensetracker/model/entities/category.dart';
 import 'package:expensetracker/model/entities/debt.dart';
 import 'package:expensetracker/model/entities/expenses.dart';
 import 'package:expensetracker/model/entities/person.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'main.g.dart';
 
-class _DataStore {
-  List<Person> person = [];
-  List<Expense> expense = [];
-  List<Debt> debt = [];
-
-  List<Category> category = [];
-  List<Budget> budget = [];
+@riverpod
+Future<Isar> expenseDatabase(Ref ref) async {
+  final dir = await getApplicationDocumentsDirectory();
+  final isar = await Isar.open(
+    [
+      PersonSchema,
+      ExpenseSchema,
+      CategorySchema,
+    ],
+    directory: dir.path,
+  );
+  return isar;
 }
 
 @riverpod
-class Database extends _$Database {
-  _DataStore _dataStore = _DataStore();
-  @override
-  Future<_DataStore> build() async {
-    return _dataStore;
-  }
+Future<Isar> debtDatabase(Ref ref) async {
+  final dir = await getApplicationDocumentsDirectory();
+  final isar = await Isar.open(
+    [
+      PersonSchema,
+      DebtSchema,
+    ],
+    directory: dir.path,
+  );
+  return isar;
+}
+
+@riverpod
+Future<Isar> categoryDatabase(Ref ref) async {
+  final dir = await getApplicationDocumentsDirectory();
+  final isar = await Isar.open(
+    [
+      CategorySchema,
+      BudgetSchema,
+    ],
+    directory: dir.path,
+  );
+  return isar;
 }
--- lib/model/main.g.dart
+++ lib/model/main.g.dart
@@ -6,20 +6,49 @@ part of 'main.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$databaseHash() => r'2069c21b72152feda4bc8749c143fc9b0c4555bc';
+String _$expenseDatabaseHash() => r'b88fa4b3d64215d3ecb4b1bb62e7c73de3617bbc';
 
-/// See also [Database].
-@ProviderFor(Database)
-final databaseProvider =
-    AutoDisposeAsyncNotifierProvider<Database, _DataStore>.internal(
-  Database.new,
-  name: r'databaseProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$databaseHash,
+/// See also [expenseDatabase].
+@ProviderFor(expenseDatabase)
+final expenseDatabaseProvider = AutoDisposeFutureProvider<Isar>.internal(
+  expenseDatabase,
+  name: r'expenseDatabaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$expenseDatabaseHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$Database = AutoDisposeAsyncNotifier<_DataStore>;
+typedef ExpenseDatabaseRef = AutoDisposeFutureProviderRef<Isar>;
+String _$debtDatabaseHash() => r'ac156daecfafa91d9573a35ecada04a57223e6d2';
+
+/// See also [debtDatabase].
+@ProviderFor(debtDatabase)
+final debtDatabaseProvider = AutoDisposeFutureProvider<Isar>.internal(
+  debtDatabase,
+  name: r'debtDatabaseProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$debtDatabaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef DebtDatabaseRef = AutoDisposeFutureProviderRef<Isar>;
+String _$categoryDatabaseHash() => r'c9be8a11a8b69c37005cf4427babc02eb3d94c52';
+
+/// See also [categoryDatabase].
+@ProviderFor(categoryDatabase)
+final categoryDatabaseProvider = AutoDisposeFutureProvider<Isar>.internal(
+  categoryDatabase,
+  name: r'categoryDatabaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$categoryDatabaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CategoryDatabaseRef = AutoDisposeFutureProviderRef<Isar>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
