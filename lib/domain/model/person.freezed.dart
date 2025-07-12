c9d96452b0af2c4dbb8fa80fbc68de353227134e
Hamburgir <shantnuprofid@gmail.com>
2025-07-13 04:51:03 +0530

    added domain model and repository for person

---
+++ lib/domain/model/person.freezed.dart
@@ -0,0 +1,283 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'person.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+/// @nodoc
+mixin _$Person {
+
+ int get id; String get firstName; String? get middleName; String? get lastName; int get number;
+/// Create a copy of Person
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$PersonCopyWith<Person> get copyWith => _$PersonCopyWithImpl<Person>(this as Person, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Person&&(identical(other.id, id) || other.id == id)&&(identical(other.firstName, firstName) || other.firstName == firstName)&&(identical(other.middleName, middleName) || other.middleName == middleName)&&(identical(other.lastName, lastName) || other.lastName == lastName)&&(identical(other.number, number) || other.number == number));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,id,firstName,middleName,lastName,number);
+
+@override
+String toString() {
+  return 'Person(id: $id, firstName: $firstName, middleName: $middleName, lastName: $lastName, number: $number)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $PersonCopyWith<$Res>  {
+  factory $PersonCopyWith(Person value, $Res Function(Person) _then) = _$PersonCopyWithImpl;
+@useResult
+$Res call({
+ int id, String firstName, String? middleName, String? lastName, int number
+});
+
+
+
+
+}
+/// @nodoc
+class _$PersonCopyWithImpl<$Res>
+    implements $PersonCopyWith<$Res> {
+  _$PersonCopyWithImpl(this._self, this._then);
+
+  final Person _self;
+  final $Res Function(Person) _then;
+
+/// Create a copy of Person
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = null,Object? firstName = null,Object? middleName = freezed,Object? lastName = freezed,Object? number = null,}) {
+  return _then(_self.copyWith(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as int,firstName: null == firstName ? _self.firstName : firstName // ignore: cast_nullable_to_non_nullable
+as String,middleName: freezed == middleName ? _self.middleName : middleName // ignore: cast_nullable_to_non_nullable
+as String?,lastName: freezed == lastName ? _self.lastName : lastName // ignore: cast_nullable_to_non_nullable
+as String?,number: null == number ? _self.number : number // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
+}
+
+}
+
+
+/// Adds pattern-matching-related methods to [Person].
+extension PersonPatterns on Person {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _Person value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _Person() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _Person value)  $default,){
+final _that = this;
+switch (_that) {
+case _Person():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _Person value)?  $default,){
+final _that = this;
+switch (_that) {
+case _Person() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( int id,  String firstName,  String? middleName,  String? lastName,  int number)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _Person() when $default != null:
+return $default(_that.id,_that.firstName,_that.middleName,_that.lastName,_that.number);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( int id,  String firstName,  String? middleName,  String? lastName,  int number)  $default,) {final _that = this;
+switch (_that) {
+case _Person():
+return $default(_that.id,_that.firstName,_that.middleName,_that.lastName,_that.number);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( int id,  String firstName,  String? middleName,  String? lastName,  int number)?  $default,) {final _that = this;
+switch (_that) {
+case _Person() when $default != null:
+return $default(_that.id,_that.firstName,_that.middleName,_that.lastName,_that.number);case _:
+  return null;
+
+}
+}
+
+}
+
+/// @nodoc
+
+
+class _Person implements Person {
+  const _Person({this.id = 0, required this.firstName, required this.middleName, required this.lastName, required this.number});
+  
+
+@override@JsonKey() final  int id;
+@override final  String firstName;
+@override final  String? middleName;
+@override final  String? lastName;
+@override final  int number;
+
+/// Create a copy of Person
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$PersonCopyWith<_Person> get copyWith => __$PersonCopyWithImpl<_Person>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Person&&(identical(other.id, id) || other.id == id)&&(identical(other.firstName, firstName) || other.firstName == firstName)&&(identical(other.middleName, middleName) || other.middleName == middleName)&&(identical(other.lastName, lastName) || other.lastName == lastName)&&(identical(other.number, number) || other.number == number));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,id,firstName,middleName,lastName,number);
+
+@override
+String toString() {
+  return 'Person(id: $id, firstName: $firstName, middleName: $middleName, lastName: $lastName, number: $number)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$PersonCopyWith<$Res> implements $PersonCopyWith<$Res> {
+  factory _$PersonCopyWith(_Person value, $Res Function(_Person) _then) = __$PersonCopyWithImpl;
+@override @useResult
+$Res call({
+ int id, String firstName, String? middleName, String? lastName, int number
+});
+
+
+
+
+}
+/// @nodoc
+class __$PersonCopyWithImpl<$Res>
+    implements _$PersonCopyWith<$Res> {
+  __$PersonCopyWithImpl(this._self, this._then);
+
+  final _Person _self;
+  final $Res Function(_Person) _then;
+
+/// Create a copy of Person
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = null,Object? firstName = null,Object? middleName = freezed,Object? lastName = freezed,Object? number = null,}) {
+  return _then(_Person(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as int,firstName: null == firstName ? _self.firstName : firstName // ignore: cast_nullable_to_non_nullable
+as String,middleName: freezed == middleName ? _self.middleName : middleName // ignore: cast_nullable_to_non_nullable
+as String?,lastName: freezed == lastName ? _self.lastName : lastName // ignore: cast_nullable_to_non_nullable
+as String?,number: null == number ? _self.number : number // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
+}
+
+
+}
+
+// dart format on
