63aba5786ad9f501338a2e2d73d37408ad320101
Hamburgir <shantnuprofid@gmail.com>
2025-12-21 16:24:25 +0530

    Added future plan notes in readme

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # expensetracker
+### TODO
+1. Division of database-per year?
+   Handle loading and unloading of databases, invalidate cache.
+
+1. Autopay
+   Enum for custom timers
+   List of expenses that is to be repeated for this
+   Autopay list - contains all autopays and last rupdate time.
+
+1. Autopay Generator
+   Generator checks date then fills up the expenses till today
+   Some kind of timer? For auto triggering generation
+
+1. Virtual expense(?)
+   very hard
+   Divide single expense for each month/day as needed
 
 A new Flutter project.
-
 ## Getting Started
-
 This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
--- lib/cubit/person_cubit.dart
+++ lib/cubit/person_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:expensetracker/domain/cache.dart';
+import 'package:expensetracker/domain/model/debt.dart';
 import 'package:expensetracker/domain/model/person.dart';
+import 'package:expensetracker/domain/repository/debt.dart';
 import 'package:expensetracker/domain/repository/person.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,9 +9,11 @@ part 'person_state.dart';
 
 class PersonCubit extends Cubit<PersonState> {
   final PersonRepo personRepo;
+  final DebtRepo debtRepo;
   final Cache cache;
   PersonCubit(
     this.personRepo,
+    this.debtRepo,
     this.cache,
   ) : super(PersonInitial()) {
     loadAll();
@@ -31,12 +35,16 @@ class PersonCubit extends Cubit<PersonState> {
     cache.people[cacheItem.value.id] = cacheItem;
   }
 
+  void _emitLoadedPeople() {
+    emit(PersonLoaded(cache.people));
+  }
+
   Future<void> loadAll() async {
     emit(PersonLoading());
     try {
       final all = await personRepo.getAll();
       all.forEach(_addToCache);
-      emit(PersonLoaded(cache.people));
+      _emitLoadedPeople();
     } catch (e) {
       emit(PersonError("$e\nFailed to load"));
     }
@@ -45,20 +53,26 @@ class PersonCubit extends Cubit<PersonState> {
   Future<void> loadOnly(int id) async {
     emit(PersonLoading());
     try {
-      emit(PersonLoading());
+      Person? person = await personRepo.getById(id);
+      if (person == null) {
+        emit(PersonError("No person with id: $id"));
+        return;
+      }
+      _addToCache(person);
+      _emitLoadedPeople();
     } catch (e) {
-      emit(PersonError("$e\nFailed to load Person with id:$id"));
+      emit(PersonError("$e\nFailed to load Person with id: $id"));
     }
   }
 
   Future<void> create(Person person) async {
     await personRepo.create(person);
-    await loadAll();
+    _emitLoadedPeople();
   }
 
   Future<void> remove(int id) async {
     await personRepo.delete(id);
-    await loadAll();
+    _emitLoadedPeople();
   }
 
   Future<void> update(Person person) async {
